afee4c3b4ec3195441c2511baca4af78c8a3f6e7
Gigi <gigisvanidze12350@Gmail.com>
2025-02-04 23:25:06 +0400

    update

--- JS/browse.js
+++
@@ -1,8 +0,0 @@
-const numberBox = document.querySelector(".number-box");
-const inputBox = document.querySelector(".input-box");
-
-numberBox.addEventListener("click", function (e) {
-  if (e.target.classList.contains("numbered-btn")) {
-    inputBox.classList.remove("hidden");
-  }
-});
